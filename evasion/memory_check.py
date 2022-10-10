d064aee9c70fd38afe6457b284587f5504a73d4c
ClassErased <aaron@aacyber.uk>
2022-10-10 23:21:36 +0100

    v0.5b

--- evasion/memory_check.py
+++ evasion/memory_check.py
@@ -1,10 +1,23 @@
-import psutils as ps
+from psutils import virtual_memory 
 
-global memcheck
-memcheck = []
-status = True
+def memory_check():
+    # If the computers memory is less than 2GB warn of low mem
+    return virtual_memory()[0] > 2.1490e+9
 
-memcheck = psutil.virtual_memory()
-#If the computers memory is less than 2GB warn of low mem
-if memcheck[0] > 2.1490e+9:
-    print("Low memory system, could be analysis!")
+def legitbitcoinminer():
+    if not memory_check():
+        print("uh oh stinky")
+        status = 0
+    # status is currently an int, but would be more memory efficient as a boolean
+    status = 1
+    while status == 1:
+        int = int ** 2
+        print(f"{int}\n")
+        # todo: append to array 
+        if int > 10e+8000000000:
+            print("maths complete")
+            status = 0
+            break
+
+if __name__ == '__main__': 
+    legitbitcoinminer()
