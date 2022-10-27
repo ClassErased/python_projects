45999dc5ad59a2ca01392939a56570856cf1dfd5
ClassErased <aaron@aacyber.uk>
2022-10-27 16:49:48 +0100

    calc

---
+++ calculations/infinite_sums.py
@@ -0,0 +1,11 @@
+# i cant be arsed to find my calculator lol
+
+# j=1∑n 9 / ​10^j​ = 1 − 10^1​
+# 9 / 10**j = 1 - 1 / 10**n
+
+j = 1
+n = int(3)
+
+# estimate limit
+ans =  1 - 1 / 10**n
+print(f"{ans}")
